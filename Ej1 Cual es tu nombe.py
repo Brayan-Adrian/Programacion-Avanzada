aaf73594d033b90c7b1869a2329e48e227172936
Brayan-Adrian <53128880+Brayan-Adrian@users.noreply.github.com>
2019-09-18 09:45:08 -0500

    Add files via upload

---
+++ Ej1 Cual es tu nombe.py
@@ -0,0 +1,5 @@
+#El comando print imprime un mensaje en la pantalla o en otro dispositivo de salida. El mansaje puede ser una cadena de caracteres o cualquier objeto que sea convertible a cadena de caracteres. 
+
+nombre=str(input('Cual es tu nombre'))
+print('Hola','Mi nombre es', nombre,'y soy muy feliz')
+
