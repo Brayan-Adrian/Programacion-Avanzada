fb185d65765b5020440cf412fce65ee4f29ee156
Brayan-Adrian <53128880+Brayan-Adrian@users.noreply.github.com>
2019-09-18 09:57:13 -0500

    Add files via upload

---
+++ Ej2 Nombre, edad, estatura.py
@@ -0,0 +1,9 @@
+#El comando print imprime un mensaje en la pantalla o en otro dispositivo de salida. El mansaje puede ser una cadena de caracteres o cualquier objeto que sea convertible a cadena de caracteres. 
+#El comando input permite al usuario introducir informacion utilizando el teclado la variable donde se guarda dicha informacion es de tipo string o cadena de caracteres
+#El comando int convierte a tipo entero
+#El comando float convierte a tipo d4ecimal
+
+nombre=input('Inserta tu nombre:')             
+edad=int(input('inserta tu edad:'))
+estatura=float(input('Inserta tu estatura:'))
+print('Hola','Mi nombre es', nombre,'naci en', 2019-edad,'y mido', estatura,'y soy muy feliz')
