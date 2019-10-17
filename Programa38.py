44a67eb98690401183c877c69a13526bb4e3b7f6
Brayan-Adrian <53128880+Brayan-Adrian@users.noreply.github.com>
2019-10-17 18:09:20 -0500

    Add files via upload

---
+++ Programa38.py
@@ -0,0 +1,42 @@
+mes=str(input('Ingresa el mes: '))
+
+
+if mes=='Enero':
+    print('Este mes tiene 31 dias')
+    
+elif mes=='Febrero':
+    print('Este mes tiene 28 dias')
+    
+elif mes=='Marzo':
+    print('Este mes tiene 31 dias')
+    
+elif mes=='Abril':
+    print('Este mes tiene 30 dias')
+    
+elif mes=='Mayo':
+    print('Este mes tiene 31 dias')
+    
+elif mes=='Junio':
+    print('Este mes tiene 30 dias')
+    
+elif mes=='Julio':
+    print('Este mes tiene 31 dias')
+    
+elif mes=='Agosto':
+    print('Este mes tiene 31 dias')
+    
+elif mes=='Septiembre':
+    print('Este mes tiene 30 dias')
+    
+elif mes=='Octubre':
+    print('Este mes tiene 31 dias')
+    
+elif mes=='Noviembre':
+    print('Este mes tiene 30 dias')
+    
+elif mes=='Diciembre':
+    print('Este mes tiene 312 dias')
+
+    
+else:
+    print('respuesta invalida')
