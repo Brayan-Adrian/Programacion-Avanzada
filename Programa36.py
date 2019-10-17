c06c9ea5bfe5cdea6cfdc5ca48c710e83387afb8
Brayan-Adrian <53128880+Brayan-Adrian@users.noreply.github.com>
2019-10-17 17:15:44 -0500

    Add files via upload

---
+++ Programa36.py
@@ -0,0 +1,85 @@
+letra=str(input('Ingrese la letra a analizar'))
+
+if letra=="a":
+    print('La letra es una vocal')
+    
+elif letra=="b":
+    print('La letra es una consonante')
+    
+elif letra=="c":
+    print('La letra es una consonante')
+    
+elif letra=="d":
+    print('La letra es una consonante')
+    
+elif letra=="e":
+    print('La letra es una vocal')
+    
+elif letra=="f":
+    print('La letra es una consonante')
+    
+elif letra=="g":
+    print('La letra es una consonante')
+    
+elif letra=="h":
+    print('La letra es una consonante')
+    
+elif letra=="i":
+    print('La letra es una vocal')
+    
+elif letra=="j":
+    print('La letra es una consonante')
+    
+elif letra=="k":
+    print('La letra es una consonante')
+    
+elif letra=="l":
+    print('La letra es una consonante')
+    
+elif letra=="m":
+    print('La letra es una consonante')
+    
+elif letra=="n":
+    print('La letra es una consonante')
+    
+elif letra=="ñ":
+    print('La letra es una consonante')
+    
+elif letra=="o":
+    print('La letra es una vocal')
+    
+elif letra=="p":
+    print('La letra es una consonante')
+    
+elif letra=="q":
+    print('La letra es una consonante')
+    
+elif letra=="r":
+    print('La letra es una consonante')
+    
+elif letra=="s":
+    print('La letra es una consonante')
+    
+elif letra=="t":
+    print('La letra es una consonante')
+    
+elif letra=="u":
+    print('La letra es una vocal')
+    
+elif letra=="v":
+    print('La letra es una consonante')
+    
+elif letra=="w":
+    print('La letra es una consonante')
+    
+elif letra=="x":
+    print('La letra es una consonante')
+    
+elif letra=="y":
+    print('La letra es una consonante')
+    
+elif letra=="z":
+    print('La letra es una consonante')
+    
+else:
+    print('letra incorrecta')
