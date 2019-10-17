c93d22c7c54f5bddb5818c97656953adbe4153f2
Brayan-Adrian <53128880+Brayan-Adrian@users.noreply.github.com>
2019-10-17 17:39:30 -0500

    Add files via upload

---
+++ Programa 37.py
@@ -0,0 +1,28 @@
+nlados=int(input('Ingrese el numero de lados de la figura'))
+
+if nlados==3:
+    print('La figura es un Triagulo')
+    
+elif nlados==4:
+    print('La figura es un Quadrilatero')
+    
+elif nlados==5:
+    print('La figura es un Pentagono')
+    
+elif nlados==6:
+    print('La figura es un Hexagono')
+    
+elif nlados==7:
+    print('La figura es un Heptagono')
+    
+elif nlados==8:
+    print('La figura es un Octagono')
+    
+elif nlados==9:
+    print('La figura es un Nonagono')
+    
+elif nlados==10:
+    print('La figura es un Decagono')
+          
+else:
+    print('numero de lados incorrecto')
