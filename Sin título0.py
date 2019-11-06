047b29a715ae9845e26fe685c3a21f9dcc0cd4ad
Brayan-Adrian <53128880+Brayan-Adrian@users.noreply.github.com>
2019-11-06 12:59:50 -0600

    Add files via upload

---
+++ Sin título0.py
@@ -0,0 +1,18 @@
+Dollar=int(input('Ingrese el valor del dolar: '))
+
+if Dollar==1:
+    print('La persona que esta plasmada en el dollar es George Washington')
+elif Dollar==2:
+    print('La persona que esta plasmada en el dollar es Thomas Jefferson')
+elif Dollar==5:
+    print('La persona que esta plasmada en el dollar es Abraham Lincoln')
+elif Dollar==10:
+    print('La persona que esta plasmada en el dollar es Alexander Hamilton')
+elif Dollar==20:
+    print('La persona que esta plasmada en el dollar es Andrew Jackson')
+elif Dollar==50:
+    print('La persona que esta plasmada en el dollar es Ulysses S. Grant')
+elif Dollar==100:
+    print('La persona que esta plasmada en el dollar es Benjamin Franklin')
+else:
+    print('El valor del dollar es incorrecto')
