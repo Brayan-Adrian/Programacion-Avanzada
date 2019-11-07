cf7b8edc4c9019248228be0e46189c2798b49f91
Brayan-Adrian <53128880+Brayan-Adrian@users.noreply.github.com>
2019-11-07 16:49:32 -0600

    Add files via upload

---
+++ programa42.py
@@ -0,0 +1,18 @@
+
+Hertz=float(input('Ingrese los Hertz de la nota: '))
+if Hertz==261.63:
+    print('La nota es C4')
+elif Hertz==293.66:
+    print('La nota es D4')
+elif Hertz==329.63:
+    print('La nota es E4')
+elif Hertz==349.23:
+    print('La nota es F4')
+elif Hertz==392.00:
+    print('La nota es G4')
+elif Hertz==440.00:
+    print('La nota es A4')
+elif Hertz==493.88:
+    print('La nota es B4')
+else:
+    print('La nota es incorrecta')
